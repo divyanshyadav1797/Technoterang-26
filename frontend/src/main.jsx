dfa8e314c92489fcae3b65eff09f6e6d8fd98aec
divyanshyadav1797 <divyanshyadav1797@gmai.com>
2026-04-18 13:49:40 +0530

    remove everything

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Ballpit from './component/Ballpit.jsx'
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    
     <App />
-  </StrictMode>,
+    
+  </StrictMode>
 )
